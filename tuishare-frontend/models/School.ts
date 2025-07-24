63081f67b47837d2a39e5c628d70bcda2a351d93
RockieRaheem <kamwangaraheem2050@gmail.com>
2025-07-24 10:45:11 +0300

    Set upmongoDB

---
+++ tuishare-frontend/models/School.ts
@@ -0,0 +1,10 @@
+import mongoose, { Schema } from "mongoose";
+
+const SchoolSchema = new Schema({
+  schoolName: { type: String, required: true },
+  schoolEmail: { type: String, required: true, unique: true },
+  schoolAddress: { type: String, required: true },
+  contactPerson: { type: String, required: true },
+});
+
+export default mongoose.models.School || mongoose.model("School", SchoolSchema);
